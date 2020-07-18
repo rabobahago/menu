6d67119aaaa55c5f8bb916b514f2fdf3a4b582ca
rabobahago <rabobahago@gmail.com>
2020-07-18 10:37:14 +0100

    filter buttons added

--- app.js
+++ app.js
@@ -74,9 +74,32 @@ const menu = [
 ];
 
 const sectionCenter = document.querySelector('.section-center');
+
+const filterBtns = document.querySelectorAll('.filter-btn');
+
+
 window.addEventListener('DOMContentLoaded', ()=>{
   displayMenuItems(menu);
 })
+
+
+filterBtns.forEach((btn)=>{
+  btn.addEventListener('click', (e)=>{
+    const category = e.currentTarget.dataset.id;
+    const menuCategory = menu.filter((menuItem)=>{
+      if(menuItem.category === category){
+        return menuItem
+      }
+    })
+    if(category === 'all'){
+      displayMenuItems(menu);  
+    }
+    else{
+      displayMenuItems(menuCategory);
+    }
+  })
+})
+
 
 function displayMenuItems(menuItems){
   let displayMenu = menuItems.map(item =>{
@@ -85,7 +108,7 @@ function displayMenuItems(menuItems){
     <div class="item-info">
       <header>
         <h4>${item.title}</h4>
-        <h4 class='price'>${item.price}</h4>
+        <h4 class='price'>$${item.price}</h4>
       </header>
       <p>
         ${item.desc}
