ffb21164b96f30fb95411ae2a9254a7a67bb9375
rabobahago <rabobahago@gmail.com>
2020-07-19 11:59:05 +0100

    final JavaScript code refactoring done

--- app.js
+++ app.js
@@ -74,30 +74,11 @@ const menu = [
 ];
 
 const sectionCenter = document.querySelector('.section-center');
-
-const filterBtns = document.querySelectorAll('.filter-btn');
-
+const container = document.querySelector('.btn-container')
 
 window.addEventListener('DOMContentLoaded', ()=>{
   displayMenuItems(menu);
-})
-
-
-filterBtns.forEach((btn)=>{
-  btn.addEventListener('click', (e)=>{
-    const category = e.currentTarget.dataset.id;
-    const menuCategory = menu.filter((menuItem)=>{
-      if(menuItem.category === category){
-        return menuItem
-      }
-    })
-    if(category === 'all'){
-      displayMenuItems(menu);  
-    }
-    else{
-      displayMenuItems(menuCategory);
-    }
-  })
+  displayMenuButtons();
 })
 
 
@@ -118,4 +99,37 @@ function displayMenuItems(menuItems){
   })
   displayMenu = displayMenu.join('');
   sectionCenter.innerHTML = displayMenu
+}
+
+const displayMenuButtons = ()=>{
+  const categories = menu.reduce((values, item)=>{
+    if(!values.includes(item.category)){
+      values.push(item.category)
+    }
+    return values
+  }, ['all']
+  );
+  const categoryBtns = categories.map((category)=>{
+    return`<button type="button" class="filter-btn" data-id=${category}>
+    ${category}
+  </button>`
+  }).join('');
+  container.innerHTML = categoryBtns;
+  const filterBtns = document.querySelectorAll('.filter-btn');
+  filterBtns.forEach((btn)=>{
+    btn.addEventListener('click', (e)=>{
+      const category = e.currentTarget.dataset.id;
+      const menuCategory = menu.filter((menuItem)=>{
+        if(menuItem.category === category){
+          return menuItem
+        }
+      })
+      if(category === 'all'){
+        displayMenuItems(menu);  
+      }
+      else{
+        displayMenuItems(menuCategory);
+      }
+    })
+  })
 }
